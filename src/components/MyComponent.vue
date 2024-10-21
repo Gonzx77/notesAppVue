3e4f00f7f19873cfa508693689048764fbda51ac
Gonzx77 <160796511+Gonzx77@users.noreply.github.com>
2024-10-21 17:51:35 -0500

    feat: :tada: Inicio del proyecto 'NotesApp'

---
+++ src/components/MyComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1 id="title">{{ message }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyComponent',
+  data(){
+    return{
+      message: 'Wellcome to the Notes App'
+    }
+  }
+}
+</script>
