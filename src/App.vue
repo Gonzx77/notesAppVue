a1b8bb6a2dcf00c8e165588013bb7478d1676123
Gonzx77 <160796511+Gonzx77@users.noreply.github.com>
2024-10-21 22:03:54 -0500

    feat: :sparkles: Ahora la app muestra las notas

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,37 @@
 <template>
-  <div id="mainGrid">
-
-<div class="mainGridSection"></div>
-<div class="mainGridSection centerV">
-    <h1 id="title">Notes</h1>
-
-    <div id="buttonsDiv">
-        <div class="buttonsDivSection center">
+    <div id="mainGrid">
+      <div class="mainGridSection"></div>
+      <div class="mainGridSection centerV">
+        <h1 id="title">Notes</h1>
+  
+        <div id="buttonsDiv">
+          <div class="buttonsDivSection center">
             <img class="menuIcon" src="../public/images/search.svg">
-        </div>
-        <div class="buttonsDivSection center">
+          </div>
+          <div class="buttonsDivSection center">
             <img class="menuIcon" src="../public/images/info.svg">
+          </div>
         </div>
+      </div>
+      
+      <div class="mainGridSection" id="notesGallery">
+        <notes />
+        
+        <div id="addBtn" class="center">
+          <img class="menuIcon" src="../public/images/add.svg">
+        </div>
+      </div>
     </div>
-</div>
-<div class="mainGridSection">
-    <div id="noNotesYet">
-        <img id="noNotesImg" src="../public/images/noNotes.svg">
-        <p id="noNotesText">Create your first note !</p>
-    </div>
-
-    <div id="addBtn" class="center">
-        <img class="menuIcon" src="../public/images/add.svg">
-    </div>
-</div>
-
-</div>
-</template>
-
-<script>
-
-export default {
-  name: 'App'
-}
-
-</script>
+  </template>
+  
+  <script>
+  import Notes from './components/notes.vue';
+  
+  export default {
+    name: 'App',
+    components: {
+      Notes 
+    }
+  }
+  </script>
+  
---
+++ src/components/notes.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <div v-if="notes.length > 0">
+      <div
+        v-for="note in notes"
+        :key="note._id"
+        class="noteDiv"
+        :style="{ backgroundColor: getRandomCreamColor() }"
+      >
+        <h1 class="noteTitle">{{ note.title }}</h1>
+      </div>
+    </div>
+    
+    <div v-else id="noNotesYet">
+      <img id="noNotesImg" src="../../public/images/noNotes.svg" alt="No notes">
+      <p id="noNotesText">Create your first note!</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'notes',
+  data() {
+    return {
+      notes: []
+    };
+  },
+  async mounted() {
+    try {
+      const response = await fetch('http://localhost:5000/notes');
+      if (!response.ok) throw new Error('Error en la red');
+      const data = await response.json();
+      this.notes = data;
+    } catch (error) {
+      console.error('Error al obtener notas:', error);
+    }
+  },
+  methods: {
+      getRandomCreamColor() {
+    const r = Math.floor(Math.random() * 56 + 150);
+    const g = Math.floor(Math.random() * 56 + 150);
+    const b = Math.floor(Math.random() * 56 + 100);
+    return `rgb(${r}, ${g}, ${b})`;
+  }
+  }
+}
+</script>
+
