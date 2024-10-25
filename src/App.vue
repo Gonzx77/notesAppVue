1fd1b7550c201e167f51f707d6a2356ec92acd4d
Gonzx77 <160796511+Gonzx77@users.noreply.github.com>
2024-10-25 06:28:16 -0500

    feat: :sparkles: Avance en la barra de busqueda

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,43 @@
 <template>
-    <div id="mainGrid">
-      <div class="mainGridSection"></div>
-      <div class="mainGridSection centerV">
-        <h1 id="title">Notes</h1>
-  
-        <div id="buttonsDiv">
-          <div class="buttonsDivSection center" id="searchBtn">
-            <img class="menuIcon" src="../public/images/search.svg">
-          </div>
-          <div class="buttonsDivSection center" id="infoBtn">
-            <img class="menuIcon" src="../public/images/info.svg">
-          </div>
+  <div id="mainGrid">
+    <div class="mainGridSection"></div>
+    <div class="mainGridSection centerV">
+      <h1 id="title" v-show="isTitleVisible">Notes</h1>
+      <input type="text" id="searchBar" v-show="!isTitleVisible" placeholder="Search note">
+
+      <div id="buttonsDiv">
+        <div class="buttonsDivSection center" id="searchBtn" @click="toggleSearch">
+          <img class="menuIcon" src="../public/images/search.svg">
+        </div>
+        <div class="buttonsDivSection center" id="infoBtn">
+          <img class="menuIcon" src="../public/images/info.svg">
         </div>
       </div>
-      
-      <div class="mainGridSection" id="notesGallery">
-        <notes />
-        
-      </div>
     </div>
-  </template>
-  
-  <script>
-  import Notes from './components/notes.vue';
-  
-  export default {
-    name: 'App',
-    components: {
-      Notes 
+    
+    <div class="mainGridSection" id="notesGallery">
+      <notes />
+    </div>
+  </div>
+</template>
+
+<script>
+import Notes from './components/notes.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Notes 
+  },
+  data() {
+    return {
+      isTitleVisible: true
+    };
+  },
+  methods: {
+    toggleSearch() {
+      this.isTitleVisible = !this.isTitleVisible;
     }
   }
-  </script>
-  
+}
+</script>
